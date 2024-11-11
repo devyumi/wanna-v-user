2e52725552f4edbd21af76e4748ea3040ca2a902
LeeJiEun <gsilverg197@gmail.com>
2024-11-11 23:55:19 +0900

    ✨Feat: implemented product List UI

---
+++ src/main/resources/static/js/product/index.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function () {
+  async function productList() {
+
+    const response = await fetch('/api/product/list');
+    if (!response.ok) {
+      throw new Error(`네트워크 오류: ${response.status} ${response.statusText}`);
+    }
+
+    const jsonData = await response.json();
+    const dataList = jsonData.data.dataList || [];
+
+    if (dataList.length > 0) {
+
+      const formattedProducts = dataList.map((product) => ({
+            ...product,
+            formattedSellingPrice: new Intl.NumberFormat("ko-KR").format(
+                product.sellingPrice) + "원",
+            formattedFinalPrice: new Intl.NumberFormat("ko-KR").format(
+                product.finalPrice) + "원",
+          })
+      );
+
+      renderProducts(formattedProducts);
+    }
+  }
+
+  productList();
+
+  const maxLength = 21;
+  const truncateText = (text) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + " ...";
+    }
+    return text;
+  };
+
+  function renderProducts(products) {
+    const productGrid = document.getElementById("product-grid");
+    productGrid.innerHTML = '';
+
+    products.forEach(product => {
+      const productItem = document.createElement('div');
+      productItem.classList.add('product-item');
+
+      const productLink = document.createElement('a');
+      productLink.href = `/products?id=${product.id}`;
+
+      const productImage = document.createElement('img');
+      productImage.src = product.image[0];
+      productImage.alt = 'Product image';
+
+      const productPriceContainer = document.createElement('div');
+      productPriceContainer.classList.add('product-price-container');
+
+      const discountRate = document.createElement('span');
+      discountRate.classList.add('product-discount-rate');
+      discountRate.textContent = `${product.discountRate}%`;
+
+      const priceContainer = document.createElement('div');
+      priceContainer.classList.add('price-container');
+
+      const sellingPrice = document.createElement('span');
+      sellingPrice.classList.add('selling-price');
+      sellingPrice.textContent = product.formattedSellingPrice;
+
+      const finalPrice = document.createElement('span');
+      finalPrice.classList.add('final-price');
+      finalPrice.textContent = product.formattedFinalPrice;
+
+      const productName = document.createElement('p');
+      productName.classList.add('product-name');
+      productName.textContent = truncateText(product.name);
+
+      priceContainer.append(sellingPrice, finalPrice);
+      productPriceContainer.append(discountRate, priceContainer);
+      productLink.append(productImage, productPriceContainer, productName);
+      productItem.append(productLink);
+
+      productGrid.append(productItem);
+    });
+  }
+
+});
+
