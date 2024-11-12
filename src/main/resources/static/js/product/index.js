b4a62a740f2e8ed6d193d81077c96ac9b589ecac
LeeJiEun <gsilverg197@gmail.com>
2024-11-12 12:29:19 +0900

    🔨Update: Change product list page rendering from CSR to SSR

--- src/main/resources/static/js/product/index.js
+++ src/main/resources/static/js/product/index.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const jsonData = await response.json();
-    const dataList = jsonData.data.dataList || [];
+    const dataList = jsonData.data || [];
 
     if (dataList.length > 0) {
 
@@ -20,19 +20,45 @@ document.addEventListener('DOMContentLoaded', function () {
           })
       );
 
-      renderProducts(formattedProducts);
+      // renderProducts(formattedProducts);
     }
   }
 
   productList();
 
+  /**
+   * 가격을 한국 원화(KRW) 형식으로 포맷팅
+   * @param price - 포맷팅할 가격
+   *  @returns string - 원화 형식으로 포맷팅된 가격 (예: "1,000원")
+   */
+  function formatPrice(price) {
+    return new Intl.NumberFormat("ko-KR").format(price) + "원";
+  }
+
+  const priceElements = document.querySelectorAll(".price");
+  priceElements.forEach(function (priceElement) {
+    const price = parseInt(priceElement.getAttribute("data-price"));
+    priceElement.textContent = formatPrice(price);
+  })
+
+
+  /**
+   * 상품명 말줄임표 형식으로 포맷팅
+   * @type {number}
+   */
   const maxLength = 21;
-  const truncateText = (text) => {
+  function truncateText(text) {
     if (text.length > maxLength) {
       return text.substring(0, maxLength) + " ...";
     }
     return text;
-  };
+  }
+
+  const nameElements = document.querySelectorAll(".name");
+  nameElements.forEach(function (nameElement) {
+    const name = nameElement.getAttribute("data-name");
+    nameElement.textContent = truncateText(name);
+  })
 
   function renderProducts(products) {
     const productGrid = document.getElementById("product-grid");
