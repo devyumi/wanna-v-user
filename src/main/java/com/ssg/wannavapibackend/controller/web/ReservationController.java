318020c0cf996eecaa7c8966a1a15770385218e7
nam3280 <nam3280@gmail.com>
2024-11-13 17:00:31 +0900

    🚀 chore: upgrade npm packages

--- src/main/java/com/ssg/wannavapibackend/controller/web/ReservationController.java
+++ src/main/java/com/ssg/wannavapibackend/controller/web/ReservationController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReservationController {
     @GetMapping("/")
     public String test() {
-        return "promotion/event";
+        return "promotion/events";
     }
 }
---
+++ src/main/java/com/ssg/wannavapibackend/service/ReservationService.java
@@ -0,0 +1,9 @@
+package com.ssg.wannavapibackend.service;
+
+import com.ssg.wannavapibackend.domain.Reservation;
+
+import java.util.List;
+
+public interface ReservationService {
+    //List<ReservationResponseDTO> getReservationList();
+}
