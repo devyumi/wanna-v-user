92f160f6ce761fc3b8fd672fe0a8e6fe6a9db7ea
devyumi <yyumiwing@naver.com>
2024-11-14 10:09:57 +0900

    feat: 마이페이지 찜 목록 조회 기능 추가

--- src/main/java/com/ssg/wannavapibackend/service/serviceImpl/MyPageServiceImpl.java
+++ src/main/java/com/ssg/wannavapibackend/service/serviceImpl/MyPageServiceImpl.java
@@ -3,20 +3,24 @@ package com.ssg.wannavapibackend.service.serviceImpl;
 import com.ssg.wannavapibackend.domain.Address;
 import com.ssg.wannavapibackend.domain.User;
 import com.ssg.wannavapibackend.dto.request.MyPageUpdateDTO;
+import com.ssg.wannavapibackend.dto.response.MyLikesResponseDTO;
 import com.ssg.wannavapibackend.dto.response.MyPageResponseDTO;
 import com.ssg.wannavapibackend.repository.UserRepository;
+import com.ssg.wannavapibackend.repository.mypage.query.MyLikesDTORepository;
 import com.ssg.wannavapibackend.repository.mypage.query.MyPageDTORepository;
 import com.ssg.wannavapibackend.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class MyPageServiceImpl implements MyPageService {
 
     private final UserRepository userRepository;
     private final MyPageDTORepository myPageDTORepository;
+    private final MyLikesDTORepository myLikesDTORepository;
 
     /**
      * 마이페이지 메인 조회
@@ -55,5 +59,16 @@ public class MyPageServiceImpl implements MyPageService {
                 .updatedAt(LocalDateTime.now())
                 .unregisteredAt(user.getUnregisteredAt())
                 .build());
+    }
+
+    /**
+     * 마이페이지 찜 목록 조회
+     *
+     * @param userId
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<MyLikesResponseDTO> findMyLikes(Long userId) {
+        return myLikesDTORepository.findMyLikesById(userId);
     }
 }
