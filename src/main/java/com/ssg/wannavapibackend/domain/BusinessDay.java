3c6405762263f524fef74ff664d7f88aba663a54
ceo92 <king9243@naver.com>
2024-11-18 17:41:51 +0900

    refactor: BusinessDay 휴무일 추가

--- src/main/java/com/ssg/wannavapibackend/domain/BusinessDay.java
+++ src/main/java/com/ssg/wannavapibackend/domain/BusinessDay.java
@@ -8,11 +8,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Entity
 @Getter @Setter(AccessLevel.PRIVATE)
@@ -23,21 +24,21 @@ public class BusinessDay {
 
   private String dayOfWeek; //요일
 
+  @DateTimeFormat(pattern = "hh:mm" , iso = ISO.TIME)
+  private LocalTime openTime; //영업 시작 시간
 
-  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
-  private LocalDateTime openTime; //영업 시작 시간
+  @DateTimeFormat(pattern = "hh:mm" , iso = ISO.TIME)
+  private LocalTime closeTime; //종료 시간
 
-  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
-  private LocalDateTime closeTime; //종료 시간
+  @DateTimeFormat(pattern = "hh:mm", iso = ISO.TIME)
+  private LocalTime breakStartTime; //브레이크 댄스 타임 시작 시간
 
-  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
-  private LocalDateTime breakStartTime; //브레이크 댄스 타임 시작 시간
+  @DateTimeFormat(pattern = "hh:mm", iso = ISO.TIME)
+  private LocalTime breakEndTime; //브레이크 댄스 타임 종료 시간
 
-  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
-  private LocalDateTime breakEndTime; //브레이크 댄스 타임 종료 시간
+  @DateTimeFormat(pattern = "hh:mm" , iso = ISO.TIME)
+  private LocalTime lastOrderTime; //라스트 오더 시간
 
-  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
-  private LocalDateTime lastOrderTime; //라스트 오더 시간
   private Boolean isClose; //문 닫는 날인지 , 이때는 전부 null 값임
 
   @ManyToOne(fetch = FetchType.LAZY)
