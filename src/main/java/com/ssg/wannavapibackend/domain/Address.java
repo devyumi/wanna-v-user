1b96ce8cb55ee9f707036bebe4d58f31a4ad981c
ceo92 <king9243@naver.com>
2024-11-18 17:41:15 +0900

    refactor: Address VO 상세 주소 컨벤션 변경

--- src/main/java/com/ssg/wannavapibackend/domain/Address.java
+++ src/main/java/com/ssg/wannavapibackend/domain/Address.java
@@ -2,17 +2,19 @@ package com.ssg.wannavapibackend.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
 @Getter @Setter
 @AllArgsConstructor
+@EqualsAndHashCode // 값 타입 비교 ==비교 가능하게끔
 public class Address {
 
   private String roadAddress;
   private String landLotAddress;
-  private String detailAddress;
+  private String detailsAddress;
   private String zipCode;
 
 
