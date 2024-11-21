e0736eeb92e853a65cbbdd543d36d8d30a557871
LeeJiEun <gsilverg197@gmail.com>
2024-11-21 11:17:44 +0900

    ✨ Feat: Implement query to fetch cart items for payment with price calculation

--- src/main/java/com/ssg/wannavapibackend/repository/PaymentRepository.java
+++ src/main/java/com/ssg/wannavapibackend/repository/PaymentRepository.java
@@ -1,7 +1,10 @@
 package com.ssg.wannavapibackend.repository;
 
 import com.ssg.wannavapibackend.domain.Payment;
+import com.ssg.wannavapibackend.dto.response.PaymentItemResponseDTO;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,12 @@ import java.util.List;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     List<Payment> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+
+   @Query("SELECT new com.ssg.wannavapibackend.dto.response.PaymentItemResponseDTO(c.id, p.image, p.name, c.quantity, " +
+       "CAST(COALESCE(p.finalPrice, 0.0) * c.quantity AS double) ) " +
+       "FROM User u " +
+       "JOIN Cart c ON u.id = c.user.id " +
+       "JOIN Product p ON c.product.id = p.id " +
+       "WHERE u.id = :userId AND c.id IN :cartIds")
+    List<PaymentItemResponseDTO> findCartsForPayment(@Param("userId") Long userId, @Param("cartIds") List<Long> cartIds);
 }
--- src/main/java/com/ssg/wannavapibackend/repository/UserCouponRepository.java
+++ src/main/java/com/ssg/wannavapibackend/repository/UserCouponRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
 
     @Query("SELECT uc FROM UserCoupon uc " +
-            "JOIN uc.coupon c " +
-            "JOIN c.event e " +
-            "WHERE uc.user.id = :userId " +
-            "AND e.endDate >= CURRENT_TIMESTAMP " +
-            "AND uc.used = false")
-    List<UserCoupon> findAllByUserIdAndEndDate(Long userId);
+        "JOIN uc.coupon c " +
+        "WHERE uc.user.id = :userId " +
+        "AND c.active = true " +
+        "AND c.endDate >= CURRENT_TIMESTAMP " +
+        "AND uc.used = false")
+    List<UserCoupon> findAllByUserIdAndEndDate(@Param("userId") Long userId);
 }
