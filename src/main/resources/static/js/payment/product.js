4d6fcd0f161130ae42a6095ad7964d628f8264b4
LeeJiEun <gsilverg197@gmail.com>
2024-11-21 20:58:26 +0900

    ✨ Feat: Display actual session values on the checkout page when navigating from the cart or product page

--- src/main/resources/static/js/payment/product.js
+++ src/main/resources/static/js/payment/product.js
@@ -1,54 +1,6 @@
 import {formatPrice, formatPriceElements} from "/js/common/format.js";
 
-// 초기 데이터 설정
-const paymentItem = {
-  point: 10000,
-  productList: [
-    {
-      productId: 2,
-      productName: "채소 랩 샌드위치",
-      productImage: "https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/950531282389413-5a75db2d-00d6-4e81-8caf-9a0085b15921.jpg",
-      productFinalPrice: 6375,
-      quantity: 2,
-    },
-    {
-      productId: 21,
-      productName: "단호박 리조또",
-      productImage: "https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/950531282389413-5a75db2d-00d6-4e81-8caf-9a0085b15921.jpg",
-      productFinalPrice: 8500,
-      quantity: 8,
-    },
-  ],
-  couponList: [
-    {
-      couponId: 3,
-      eventTitle: "11월을 시작하는 이벤트 쿠폰! 11월을 시작하는 이벤트 쿠폰!",
-      couponCode: 2024110803,
-      couponType: 'F',
-      discountAmount: 2500,
-      discountRate: 0,
-      eventEndDate: "2024-11-13",
-    },
-    {
-      couponId: 2,
-      eventTitle: "11월을 마무리하는 이벤트 쿠폰! 11월을 마무리하는 이벤트 쿠폰!",
-      couponCode: 2024110802,
-      couponType: 'P',
-      discountAmount: 0,
-      discountRate: 13,
-      eventEndDate: "2024-11-30",
-    },
-    {
-      couponId: 1,
-      eventTitle: "아무 이유 없다. 그냥 주는대로 받아라 쿠폰!",
-      couponCode: 2024110801,
-      couponType: 'F',
-      discountAmount: 1000,
-      discountRate: 0,
-      eventEndDate: "2024-11-15",
-    },
-  ],
-};
+    console.log("pageInitData: \n", JSON.stringify(pageInitData))
 /**
  * 결제 페이지 초기 배송지
  * @type {number}
@@ -68,11 +20,11 @@ if (roadAddress !== '' && zipCode !== '') {
 document.getElementById('detail-address').value = detailAddress;
 
 /**
- * 상품별 결제 가격
+ * 최초 결제 가격
  * @type {number}
  */
-const totalPrice = paymentItem.productList.reduce((total, item) => {
-  return total + (item.productFinalPrice * item.quantity);
+const totalPrice = pageInitData.products.reduce((total, item) => {
+  return total + item.paymentPrice;
 }, 0);
 
 formatPriceElements();
@@ -94,7 +46,7 @@ document.getElementById('find-address-btn').addEventListener("click",
 /**
  * 결제 상품 데이터
  */
-const productList = paymentItem.productList;
+const productList = pageInitData.products;
 const productContainer = document.querySelector("#product-container");
 
 productContainer.innerHTML = '';
@@ -105,15 +57,13 @@ productList.forEach(item => {
 
   productItem.innerHTML = `
     <div class="img-container">
-      <img src="${item.productImage}" alt="Product image"/>
+      <img src="${item.image}" alt="Product image"/>
     </div>
     <div class="product-info-container">
-      <p class="product-name">${item.productName}</p>
+      <p class="product-name">${item.name}</p>
       <div class="price-container">
         <span class="product-quantity">수량 ${item.quantity}개</span>
-        <span class="final-price price" data-price="${item.productFinalPrice
-  * item.quantity}">${(item.productFinalPrice
-      * item.quantity).toLocaleString()} 원</span>
+        <span class="final-price price" data-price="${item.paymentPrice}">${(item.paymentPrice).toLocaleString()} 원</span>
       </div>
     </div>
   `;
@@ -124,7 +74,7 @@ productList.forEach(item => {
 /**
  * 적용 가능한 쿠폰 데이터
  */
-const couponList = paymentItem.couponList;
+const couponList = pageInitData.coupons;
 const couponContainer = document.querySelector("#coupon-container");
 
 couponContainer.innerHTML = '';
@@ -135,12 +85,12 @@ couponList.forEach(item => {
 
   couponItem.innerHTML = `
     <div class="coupon-info-container">
-      <h5 class="coupon-discount-price">${item.couponType === 'F' ? formatPrice(
+      <h5 class="coupon-discount-price">${item.type === 'FIXED' ? formatPrice(
       item.discountAmount) : item.discountRate + '%'}</h5>
-      <p class="event-name">${item.eventTitle}</p>
-      <p class="event-period">~ ${item.eventEndDate} 까지</p>
+      <p class="event-name">${item.name}</p>
+      <p class="event-period">~ ${item.endDate} 까지</p>
     </div>
-    <div class="btn-select" data-coupon-id="${item.couponId}">선택</div>
+    <div class="btn-select" data-coupon-id="${item.id}">선택</div>
   `;
 
   couponContainer.appendChild(couponItem);
@@ -158,12 +108,10 @@ function chooseCoupon(couponId) {
     offcanvas.hide();
   }
 
-  const selectedCoupon = paymentItem.couponList.find(
+  const selectedCoupon = pageInitData.coupons.find(
       coupon => {
-        return coupon.couponId === parseInt(couponId);
+        return coupon.id === parseInt(couponId);
       });
-
-  console.log("selectedCoupon: " + selectedCoupon)
 
   /**
    * 쿠폰 할인 금액 계산
@@ -172,9 +120,9 @@ function chooseCoupon(couponId) {
    */
   let discountAmount = 0;
   if (selectedCoupon) {
-    if (selectedCoupon.couponType === 'F') {
+    if (selectedCoupon.type === 'FIXED') {
       discountAmount = selectedCoupon.discountAmount;
-    } else if (selectedCoupon.couponType === 'P') {
+    } else if (selectedCoupon.type === 'PERCENTAGE') {
       discountAmount = Math.ceil(
           (totalPrice * (selectedCoupon.discountRate / 100)) / 10) * 10;  // 10의 자리에서 올림
     }
@@ -206,13 +154,13 @@ couponButtons.forEach(button => {
  * 보유 포인트
  */
 document.getElementById('points-balance').innerText = '보유 ' + formatPrice(
-    paymentItem.point);
+    pageInitData.point);
 
 /**
  * 사용 포인트 입력 시 최대 포인트 제한
  */
 const pointInput = document.getElementById('used-points'); // 포인트 입력 값
-const maxPoint = paymentItem.point; // 보유 포인트 값
+const maxPoint = pageInitData.point; // 보유 포인트 값
 
 pointInput.addEventListener('input', function () {
   // 1. 숫자만 추출
