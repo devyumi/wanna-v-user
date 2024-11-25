50c5d88c54bcfd5ab9e4d9d647bc82b32f868fe7
devyumi <yyumiwing@naver.com>
2024-11-25 20:14:37 +0900

    feat: 회원 포인트 업데이트 기능 추가

--- src/main/java/com/ssg/wannavapibackend/repository/UserRepository.java
+++ src/main/java/com/ssg/wannavapibackend/repository/UserRepository.java
@@ -2,7 +2,14 @@ package com.ssg.wannavapibackend.repository;
 
 import com.ssg.wannavapibackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("UPDATE User u " +
+            "SET u.point = u.point + :point " +
+            "WHERE u.id = :userId")
+    void updateUserPoint(Long userId, Long point);
 }
