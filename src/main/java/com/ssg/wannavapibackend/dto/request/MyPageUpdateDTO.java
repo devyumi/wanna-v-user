86580dab19e54f7165934d6fd0b08c8c83e5a270
devyumi <yyumiwing@naver.com>
2024-11-25 19:28:36 +0900

    refactor: 마이페이지 수정 메서드 address 리팩토링

--- src/main/java/com/ssg/wannavapibackend/dto/request/MyPageUpdateDTO.java
+++ src/main/java/com/ssg/wannavapibackend/dto/request/MyPageUpdateDTO.java
@@ -1,5 +1,7 @@
 package com.ssg.wannavapibackend.dto.request;
 
+import com.ssg.wannavapibackend.domain.Address;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -11,16 +13,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MyPageUpdateDTO {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     @Size(min = 2, max = 10, message = "2자 이상 10자 이하만 가능합니다.")
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣]+$", message = "한글만 가능합니다.")
     private String name;
 
-    private String zipCode;
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Size(min = 2, max = 5, message = "2자 이상 5자 이하만 가능합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$", message = "한글, 영어, 숫자만 가능합니다.")
+    private String username;
 
-    private String roadAddress;
-
-    private String landLotAddress;
-
-    @Size(max = 100, message = "100자 이하만 가능합니다.")
-    private String detailAddress;
+    private Address address;
 }
