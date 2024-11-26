6594ba4c5ec0e7f295dc82d23bd532185e8b3487
devyumi <yyumiwing@naver.com>
2024-11-26 19:02:38 +0900

    feat: 예약 취소 기능 추가

--- src/main/java/com/ssg/wannavapibackend/service/serviceImpl/MyPageServiceImpl.java
+++ src/main/java/com/ssg/wannavapibackend/service/serviceImpl/MyPageServiceImpl.java
@@ -1,6 +1,9 @@
 package com.ssg.wannavapibackend.service.serviceImpl;
 
+import com.ssg.wannavapibackend.common.PaymentCancelReason;
+import com.ssg.wannavapibackend.common.Status;
 import com.ssg.wannavapibackend.domain.*;
+import com.ssg.wannavapibackend.dto.PaymentRefundDTO;
 import com.ssg.wannavapibackend.dto.request.MyPageUpdateDTO;
 import com.ssg.wannavapibackend.dto.request.MyReservationRequestDTO;
 import com.ssg.wannavapibackend.dto.response.MyLikesResponseDTO;
@@ -9,17 +12,21 @@ import com.ssg.wannavapibackend.repository.*;
 import com.ssg.wannavapibackend.repository.mypage.query.MyLikesDTORepository;
 import com.ssg.wannavapibackend.repository.mypage.query.MyPageDTORepository;
 import com.ssg.wannavapibackend.service.MyPageService;
+import com.ssg.wannavapibackend.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MyPageServiceImpl implements MyPageService {
 
+    private final PaymentService paymentService;
     private final UserRepository userRepository;
     private final MyPageDTORepository myPageDTORepository;
     private final MyLikesDTORepository myLikesDTORepository;
@@ -111,6 +118,90 @@ public class MyPageServiceImpl implements MyPageService {
     @Transactional(readOnly = true)
     public Reservation findMyReservation(Long reservationId) {
         return reservationRepository.findById(reservationId).get();
+    }
+
+    /**
+     * 예약 취소: 결제 취소 -> 예약 취소 (Payment, Reservation Update)
+     *
+     * @param reservationId
+     * @return
+     */
+    @Transactional
+    public void updateMyReservationStatus(Long reservationId) {
+        Reservation reservation = reservationRepository.findById(reservationId).get();
+        Payment payment = reservation.getPayment();
+        Double cancelAmount = calculateCancelAmount(reservation);
+
+        PaymentRefundDTO paymentRefundDTO = PaymentRefundDTO.builder()
+                .paymentKey(payment.getPaymentKey())
+                .cancelReason(PaymentCancelReason.RESERVATION_CANCEL)
+                .cancelAmount(cancelAmount)
+                .build();
+
+        PaymentRefundDTO result = paymentService.requestPaymentCancel(paymentRefundDTO);
+
+        //결제 취소 성공 시 Update
+        String canceledAt = result.getCancels().get(0).getCanceledAt();
+        LocalDateTime canceledAtToLocalDateTime = OffsetDateTime.parse(canceledAt).toLocalDateTime();
+
+        //Payment Update
+        paymentRepository.save(Payment.builder()
+                .id(payment.getId())
+                .user(payment.getUser())
+                .paymentKey(payment.getPaymentKey())
+                .orderId(payment.getOrderId())
+                .actualPrice(payment.getActualPrice())
+                .finalPrice(payment.getFinalPrice())
+                .pointsUsed(payment.getPointsUsed())
+                .finalDiscountRate(payment.getFinalDiscountRate())
+                .finalDiscountAmount(payment.getFinalDiscountAmount())
+                .couponCode(payment.getCouponCode())
+                .status(Status.CANCELED)
+                .address(payment.getAddress())
+                .note(payment.getNote())
+                .cancelReason(PaymentCancelReason.RESERVATION_CANCEL.toString())
+                .cancelAmount(cancelAmount)
+                .createdAt(payment.getCreatedAt())
+                .approvedAt(payment.getApprovedAt())
+                .canceledAt(canceledAtToLocalDateTime)
+                .build());
+
+        //Reservation Update
+        reservationRepository.save(Reservation.builder()
+                .id(reservationId)
+                .user(reservation.getUser())
+                .restaurant(reservation.getRestaurant())
+                .payment(payment)
+                .guest(reservation.getGuest())
+                .reservationDate(reservation.getReservationDate())
+                .reservationTime(reservation.getReservationTime())
+                .createdAt(reservation.getCreatedAt())
+                .updatedAt(LocalDateTime.now())
+                .status(false)
+                .build());
+    }
+
+    /**
+     * 위약금 계산
+     *
+     * @param reservation
+     * @return
+     */
+    public Double calculateCancelAmount(Reservation reservation) {
+        double deposit = 10000 * reservation.getGuest();
+        LocalDate reservationDate = reservation.getReservationDate();
+        //예약 2일 전: 100% 환불
+        if (LocalDate.now().minusDays(2).isEqual(reservationDate) || LocalDate.now().minusDays(2).isAfter(reservationDate)) {
+            return deposit;
+
+            //예약 1일 전: 50% 환불
+        } else if (LocalDate.now().minusDays(1).isEqual(reservationDate) || LocalDate.now().minusDays(1).isAfter(reservationDate)) {
+            return deposit * 0.5;
+
+            //예약 30분 전 or 노쇼: 환불 0%
+        } else {
+            return 0.0;
+        }
     }
 
     /**
