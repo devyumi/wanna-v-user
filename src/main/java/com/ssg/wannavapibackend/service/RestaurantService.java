fc4938deb62a8ad1e61ad5975d26ebbbba4cba49
ceo92 <king9243@naver.com>
2024-12-04 17:47:25 +0900

    refactor: RestaurantService 리팩토링

--- src/main/java/com/ssg/wannavapibackend/service/RestaurantService.java
+++ src/main/java/com/ssg/wannavapibackend/service/RestaurantService.java
@@ -3,6 +3,7 @@ package com.ssg.wannavapibackend.service;
 import com.ssg.wannavapibackend.domain.BusinessDay;
 import com.ssg.wannavapibackend.domain.Restaurant;
 import com.ssg.wannavapibackend.domain.Review;
+import com.ssg.wannavapibackend.dto.request.RestaurantAdminSearchCond;
 import com.ssg.wannavapibackend.dto.request.RestaurantSaveDTO;
 import com.ssg.wannavapibackend.dto.request.RestaurantSearchCond;
 import com.ssg.wannavapibackend.dto.request.RestaurantUpdateDTO;
@@ -19,12 +20,14 @@ public interface RestaurantService {
 
     List<Restaurant> findSimilarRestaurants(Long id);
 
-    List<Restaurant> findRestaurants(RestaurantSearchCond restaurantSearchCond);
+    List<Restaurant> findRestaurants(RestaurantSearchCond restaurantSearchCond , String search);
 
 
     BusinessDay findToday(Restaurant restaurant);
 
-    void updateRestaurant(Long id , RestaurantUpdateDTO restaurantUpdateDto);
+    void updateRestaurant(RestaurantUpdateDTO restaurantUpdateDto);
+
+    List<Restaurant> findRestaurantsAdmin(RestaurantAdminSearchCond restaurantAdminSearchCond);
 
     void updateBusinessStatus();
 
